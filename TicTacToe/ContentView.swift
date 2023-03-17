e48e1507e1ea2372ca4a69bda21eb327ccf9c36c
Noor Chasib <noorchasib@gmail.com>
2023-03-17 16:21:00 -0700

    create simple tictactoe game

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -1,26 +1,63 @@
-//
-//  ContentView.swift
-//  TicTacToe
-//
-//  Created by Noor Chasib on 2023-03-17.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+   @StateObject var gameState = GameState()
+
+   var body: some View {
+      let borderSize = CGFloat(5)
+
+      Text(gameState.turnText())
+         .font(.title)
+         .bold()
+         .padding()
+      Spacer()
+
+      Text(String(format: "X's: %d", gameState.crossesScore))
+         .font(.title)
+         .bold()
+         .padding()
+
+      VStack(spacing: borderSize) {
+         ForEach(0...2, id: \.self) {
+            row in
+            HStack(spacing: borderSize) {
+               ForEach(0...2, id: \.self) {
+                  column in
+
+                  let cell = gameState.board[row][column]
+
+                  Text(cell.displayTile())
+                     .font(.system(size: 60))
+                     .foregroundColor(cell.tileColor())
+                     .bold()
+                     .frame(maxWidth: .infinity, maxHeight: .infinity)
+                     .aspectRatio(1, contentMode: .fit)
+                     .background(Color.white)
+                     .onTapGesture {
+                        gameState.placeTile(row, column)
+                     }
+               }
+            }
+         }
+      }
+      .background(Color.black)
+      .padding()
+      .alert(isPresented: $gameState.showAlert) {
+         Alert(title: Text(gameState.alertMessage),
+               dismissButton: .default(Text("Okay")) {
+                  gameState.resetBoard()
+               })
+      }
+      Text(String(format: "O's: %d", gameState.noughtsScore))
+         .font(.title)
+         .bold()
+         .padding()
+      Spacer()
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+      ContentView()
+   }
 }
